d0ef9af3436ab2c508d58fbe30d34338c89d76a1
Nishant <nishkohli96@gmail.com>
2023-10-05 03:55:46 +0530

    upgrade tailwindcss and postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,20 @@
 module.exports = {
-    purge: {
-        enabled: true,
-        content: ['./src/**/*.js', './public/**/*.html'],
+  content: ['./src/**/*.js', './public/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        headercol: '#1F1F1F',
+        bodycol: '#2B2B2B',
+        cocowhite: '#F7F6F3',
+      },
+      screens: {
+        xs: '300px',
+        mobile: { min: '380px', max: '550px' },
+      },
     },
-    darkMode: false,
-    theme: {
-        extend: {
-            colors: {
-                headercol: '#1F1F1F',
-                bodycol: '#2B2B2B',
-                cocowhite: '#F7F6F3',
-            },
-            screens: {
-                xs: '300px',
-                mobile: { min: '380px', max: '550px' },
-            },
-        },
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
